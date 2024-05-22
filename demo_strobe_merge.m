f2606c6938035f118702ca798c0f8a0520ee2ad0
lcbarnett <l.c.barnett@sussex.ac.uk>
2024-05-22 15:06:02 +0100

    Added strobe sequence merge demo plus various tweaks

---
+++ demo_strobe_merge.m
@@ -0,0 +1,63 @@
+%%%%%%%%%%%%%%%%%% Simulation Parameters (override on command line) %%%%%%%%%%%%%%%%%%
+
+defvar('F1',      10      ); % strobe frequency 1 (Hz)
+defvar('F2',      7       ); % strobe frequency 2 (Hz)
+defvar('T1',      15      ); % total time 1 (secs)
+defvar('T2',      10      ); % total time 2 (secs)
+defvar('ondur',  'hcycle' ); % cycle "on" duration: 'hcycle' (default = half-cycle), of length (ms)
+defvar('toffs',  'splice' ); % time offsets: 'splice', 'super' or a 2-vector of time offsets
+defvar('rmode',   0       ); % "regularisation" mode; deal with flash overlaps (zero for none; see regularise_strobe.m)
+defvar('fs',      2000    ); % sampling frequency (Hz)
+defvar('dfac',    5       ); % spectral power display frequency cutoff factor
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% This script demonstrates splicing and superimposition of strobe sequences.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Create periodic strobe processess
+
+signal1 = gen_strobe_periodic(F1,T1,ondur);
+signal2 = gen_strobe_periodic(F2,T2,ondur);
+
+% Merge sequences
+
+[signal,T,Fe] = merge_strobe({signal1,signal2},[T1,T2],toffs,rmode);
+
+T = ceil(T*fs)/fs; % adjust T2 (if necessary) to ensure an *integer* number of samples
+
+[samples,ts] = sample_strobe(signal,fs,T); % sample signal at frequency fs
+
+[spower,f] = pspectrum(samples,fs,'FrequencyLimits',[0,dfac*max(F1,F2)]); % spectral power
+
+% Plot signals and power spectra
+
+if ischar(toffs)
+	stoffs = toffs;
+else
+	stoffs = sprintf('offsets = %d, %d secs',toffs(1),toffs(2));
+end
+
+figure(1); clf
+sgtitle(sprintf('\nMerged periodic sequences (%s): effective frequency = %g Hz\n',stoffs,Fe),'FontSize',14);
+
+subplot(2,1,1);
+plot(ts,samples);
+title('Merged signal','FontWeight','normal')
+xlabel('Time (secs)')
+ylabel('Luminance')
+xlim([0,T])
+ylim([-0.05,1.05])
+set(gca,'TickLength',[0,0]);
+
+subplot(2,1,2);
+plot(f,spower);
+title('Power spectral density (PSD)','FontWeight','normal')
+xlabel('Frequency (Hz)')
+ylabel('PSD')
+xline(F1,'b');
+xline(F2,'b');
+xline(Fe,'r');
+set(gca,'TickLength',[0,0]);
+grid on
--- merge_strobe.m
+++ merge_strobe.m
@@ -1,25 +1,54 @@
-function [signal,slength] = merge_strobe(signals,slengths,toffsets)
+function [signal,T,Fe] = merge_strobe(signals,Ts,toffs,rmode)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
 % This function merges/splices a set of strobe sequences.
 %
+% signals    cell vector of strobe sequences
+% Ts         Time lengths of the sequences in 'signals'
+% toffs      vector of time offsets (or special values 'super' [superpose] and 'splice')
+% rmode      regularisation mode (deal with overlapping flashes), or 0 for no regularisation (default)
+%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+if nargin < 4 || isempty(rmode), rmode =  0; end
 
 assert(iscell(signals) && isvector(signals),'Please supply a cell vector of strobe sequences');
 nsignals = length(signals);
 
-assert(isvector(slengths) && length(slengths) == nsignals && all(slengths >= 0),'Strobe sequence lengths must be a vector of nonnegative numbers matching the supplied signals');
-assert(isvector(toffsets) && length(toffsets) == nsignals && all(toffsets >= 0),'Time offsets must be a vector of nonnegative numbers matching the supplied signals');
+assert(isvector(Ts) && length(Ts) == nsignals && all(Ts >= 0),'Strobe sequence lengths must be a vector of nonnegative numbers matching the supplied signals');
+
+if ischar(toffs)
+	if     strcmpi(toffs,'super')  % superimose
+		toffs = zeros(nsignals);
+	elseif strcmpi(toffs,'splice') % splice sequentially
+		Ts = Ts(:);
+		toffs = [0;cumsum(Ts(1:end-1))];
+	else
+		error('Unknown merge mode')
+	end
+else
+	assert(isvector(toffs) && length(toffs) == nsignals && all(toffs >= 0),'Time offsets must be a vector of nonnegative numbers matching the supplied signals');
+end
 
 s = 1;
-signal = offset_strobe(signal{s},toffsets(s))];
-slength = slengths(s)+toffsets(s);
+signal = offset_strobe(signals{s},toffs(s));
+T = Ts(s)+toffs(s);
 for s = 2:nsignals
-	signal = [signal; offset_strobe(signal{s},toffsets(s))];
-	slength = max(slength,slengths(s)+toffsets(s));
+	signal = [signal; offset_strobe(signals{s},toffs(s))];
+	T = max(T,Ts(s)+toffs(s));
 end
 
 % Sort events by time stamp (in case necessary)
 
 signal = sortrows(signal);
+
+% Regularise signal (deal with overlaps)
+
+if rmode > 0
+	signal = regularise_strobe(signal,rmode);
+end
+
+% Effective frequency
+
+Fe = size(signal,1)/T;
